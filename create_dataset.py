704f9a743254a5d0501a466acde6033baea1480e
Splashing23 <dev.g@wustl.edu>
2025-01-08 05:50:53 -0500

    sync

--- create_dataset.py
+++ create_dataset.py
@@ -4,6 +4,31 @@ from io import BytesIO
 
 west, south, east, north = -95.462265, 29.676326, -95.262451, 29.815917
 
+    # https://stackoverflow.com/questions/1166059/how-can-i-get-latitude-longitude-from-x-y-on-a-mercator-map-jpeg
+    # /// <summary>
+    # /// Calculates the Y-value (inverse Gudermannian function) for a latitude. 
+    # /// <para><see cref="http://en.wikipedia.org/wiki/Gudermannian_function"/></para>
+    # /// </summary>
+    # /// <param name="latitude">The latitude in degrees to use for calculating the Y-value.</param>
+    # /// <returns>The Y-value for the given latitude.</returns>
+    # public static double GudermannianInv(double latitude)
+    # {
+    #     double sign = Math.Sign(latitude);
+    #     double sin = Math.Sin(latitude * RADIANS_PER_DEGREE * sign);
+    #     return sign * (Math.Log((1.0 + sin) / (1.0 - sin)) / 2.0);
+    # }
+
+    # /// <summary>
+    # /// Returns the Latitude in degrees for a given Y.
+    # /// </summary>
+    # /// <param name="y">Y is in the range of +PI to -PI.</param>
+    # /// <returns>Latitude in degrees.</returns>
+    # public static double Gudermannian(double y)
+    # {
+    #     return Math.Atan(Math.Sinh(y)) * DEGREES_PER_RADIAN;
+    # }
+
+
 mly_key = 'MLY|9042214512506386|3607fa048afce1dfb774b938cbf843f9'
 
 if os.path.exists('dataset'):
