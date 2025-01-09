1b723e9904fb758236cc84a349f8ee49c02fe487
Splashing23 <dev.g@wustl.edu>
2025-01-09 11:46:26 -0500

    ...

--- create_dataset.py
+++ create_dataset.py
@@ -1,65 +1,56 @@
 import requests, os, random, shutil
 from PIL import Image
 from io import BytesIO
+import math
 
+# Bounding box of entire city (currently Houston)
 west, south, east, north = -95.462265, 29.676326, -95.262451, 29.815917
 
-    # https://stackoverflow.com/questions/1166059/how-can-i-get-latitude-longitude-from-x-y-on-a-mercator-map-jpeg
-    # /// <summary>
-    # /// Calculates the Y-value (inverse Gudermannian function) for a latitude. 
-    # /// <para><see cref="http://en.wikipedia.org/wiki/Gudermannian_function"/></para>
-    # /// </summary>
-    # /// <param name="latitude">The latitude in degrees to use for calculating the Y-value.</param>
-    # /// <returns>The Y-value for the given latitude.</returns>
-    # public static double GudermannianInv(double latitude)
-    # {
-    #     double sign = Math.Sign(latitude);
-    #     double sin = Math.Sin(latitude * RADIANS_PER_DEGREE * sign);
-    #     return sign * (Math.Log((1.0 + sin) / (1.0 - sin)) / 2.0);
-    # }
+# Mapillary API token
+MLY_KEY = 'MLY|9042214512506386|3607fa048afce1dfb774b938cbf843f9'
 
-    # /// <summary>
-    # /// Returns the Latitude in degrees for a given Y.
-    # /// </summary>
-    # /// <param name="y">Y is in the range of +PI to -PI.</param>
-    # /// <returns>Latitude in degrees.</returns>
-    # public static double Gudermannian(double y)
-    # {
-    #     return Math.Atan(Math.Sinh(y)) * DEGREES_PER_RADIAN;
-    # }
-
-
-mly_key = 'MLY|9042214512506386|3607fa048afce1dfb774b938cbf843f9'
-
+# Removes existing folder and creates new one
 if os.path.exists('dataset'):
     shutil.rmtree('dataset')
-os.makedirs('dataset', exist_ok=True)
+os.makedirs('dataset', exist_ok=False)
 
+# Set number of samples to retrieve from API (does NOT get random sample; used for debugging)
 SAMPLES = 1
-ZOOM_LEVEL_18 = 0.0008 # approx
-ZOOM_LEVEL = 0.0003
-ZOOM_LEVEL_19 = 0.00004 # approx
-ZOOM_LEVEL_20 = 0.00002 # approx
+
+# In meters
+R_EARTH = 6378000 
+
+# Area of desired satellite image in square meters (100 is slightly larger than zoom level 18)
+AREA = 100 
 
 for i in range(SAMPLES):
-    coordinate = (random.uniform(east, west), random.uniform(south, north))
+    latitude, longitude= random.uniform(south, north), random.uniform(east, west) # Get random coordinate sample
+
+    # Get lattitude delta for bbox
+    lat_delta  = ((AREA / 2) / R_EARTH) * (180 / math.pi)
+    # Get longitude delta for bbox (dependent on coordinate lattitude)
+    lng_delta = ((AREA / 2) / R_EARTH) * (180 / math.pi) / math.cos(latitude * (math.pi / 180))
     
-    sample_folder = os.path.join("dataset", f"sample_{coordinate[0]}_{coordinate[1]}")
+    # Create folder for sample
+    sample_folder = os.path.join("dataset", f"sample_{latitude}_{longitude}")
     os.makedirs(sample_folder)
 
-    bbox_str = f'{coordinate[0] - ZOOM_LEVEL},{coordinate[1] - ZOOM_LEVEL},{coordinate[0] + ZOOM_LEVEL},{coordinate[1] + ZOOM_LEVEL}'
+    bbox_str = f'{longitude - lng_delta},{latitude - lat_delta},{longitude + lng_delta},{latitude + lat_delta}'
 
-    images_data_url = f'https://graph.mapillary.com/images?access_token={mly_key}&bbox={bbox_str}&is_pano=true&fields=thumb_original_url,computed_geometry'
+    images_data_url = f'https://graph.mapillary.com/images?access_token={MLY_KEY}&bbox={bbox_str}&is_pano=true&fields=thumb_original_url,computed_geometry'
 
     images_data_response = requests.get(images_data_url)
     if images_data_response.status_code == 200:
         images_data = images_data_response.json()
 
         for image_data in images_data['data']:
+            # image_id = image_data['id'] # For debugging
+
+            # Get image url and "computed"(adjusted) coordinates
             image_url = image_data['thumb_original_url']
-            image_id = image_data['id']
             image_lat, image_lng = image_data['computed_geometry']['coordinates']
             
+            # Save image
             image_bytes_response = requests.get(image_url)
             image = Image.open(BytesIO(image_bytes_response.content))
             output_path = os.path.join(sample_folder, f'panorama_{image_lat}_{image_lng}.jpeg')
--- test.py
+++ test.py
@@ -1,21 +1,75 @@
-from owslib.wms import WebMapService
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+from PIL import Image
+from io import BytesIO
 
-# Connect to GIBS WMS Service
-wms = WebMapService('https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?', version='1.1.1')
+client_id = "sh-c67860da-c051-4f35-9cb0-1f350737c4bc"
+client_secret = "TGbiLloPYOu7Op3DZi3eO6TvGvHMOrHq"
 
-# Configure request for MODIS_Terra_CorrectedReflectance_TrueColor
-img = wms.getmap(layers=['MODIS_Terra_CorrectedReflectance_TrueColor'],  # Layers
-                 srs='epsg:4326',  # Map projection
-                 bbox=(-100, 29, -90, 30),  # Bounds
-                 size=(5000, 5000),  # Image size
-                 time='2024-09-21',  # Time of data
-                 format='image/png',  # Image format
-                 transparent=True)  # Nodata transparency
+# Create a session
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
 
-# Save output PNG to a file
-out = open('MODIS_Terra_CorrectedReflectance_TrueColor.png', 'wb')
-out.write(img.read())
-out.close()
+# Get token for the session
+token = oauth.fetch_token(token_url='https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token',
+                          client_secret=client_secret, include_client_id=True)
 
-# # View image
-# Image('python-examples/MODIS_Terra_CorrectedReflectance_TrueColor.png')
+evalscript = """
+//VERSION=3
+function setup() {
+  return {
+    input: ["VV"],
+    output: { id: "default", bands: 1 },
+  }
+}
+
+function evaluatePixel(samples) {
+  return [2 * samples.VV]
+}
+"""
+
+request = {
+    "input": {
+        "bounds": {
+            "bbox": [
+                100000,
+                5000000,
+                200000,
+                6000000,
+            ],
+            "properties": {"crs": "http://www.opengis.net/def/crs/EPSG/0/3857"},
+        },
+        "data": [
+            {
+                "type": "sentinel-1-grd",
+                "dataFilter": {
+                    "timeRange": {
+                        "from": "2019-02-02T00:00:00Z",
+                        "to": "2019-04-02T23:59:59Z",
+                    }
+                },
+                "processing": {"orthorectify": "true"},
+            }
+        ],
+    },
+    "output": {
+        "width": 512,
+        "height": 512,
+        "responses": [
+            {
+                "identifier": "default",
+                "format": {"type": "image/png"},
+            }
+        ],
+    },
+    "evalscript": evalscript,
+}
+
+url = "https://sh.dataspace.copernicus.eu/api/v1/process"
+response = oauth.post(url, json=request)
+image = Image.open(BytesIO(response.content))
+image.convert('RGB').save("test.jpeg", 'JPEG')
+
+# image_bytes_response = oauth.get("https://sh.dataspace.copernicus.eu/api/v1/catalog/1.0.0/collections/sentinel-1-grd/items/S1B_IW_GRDH_1SDV_20191210T051027_20191210T051052_019298_0246FE_ED7D_COG.SAFE")
+# print(image_bytes_response.content)
+# image = Image.open(BytesIO(image_bytes_response.content))
