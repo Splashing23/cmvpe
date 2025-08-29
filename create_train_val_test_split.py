7acf352eeac9f0c063de057f5e9100047cf218ba
Dev Gupta <dev.g@wustl.edu>
2025-08-29 00:40:16 -0500

    ...

--- create_train_val_test_split.py
+++ create_train_val_test_split.py
@@ -1,10 +1,13 @@
 import os
 import random
 import folium
+import pandas as pd
 from folium.plugins import HeatMap
 import geopandas as gpd
 from shapely.geometry import box
 import numpy as np
+
+
 
 cities = {
     # Dense ground mapillary data
@@ -54,104 +57,123 @@ heat_data = set()
 for city in os.listdir(os.path.join("dataset", "splits")):
     unused_count = 0
     city_splits_path = os.path.join("dataset", "splits", city)
-    with (
-        open(os.path.join(city_splits_path, "samples.csv"), "r") as f_samples, 
-        open(os.path.join(city_splits_path, "train.csv"), "w") as f_train, 
-        open(os.path.join(city_splits_path, "validation.csv"), "w") as f_val,
-        open(os.path.join(city_splits_path, "test.csv"), "w") as f_test,
-    ):
-        long_unit = (cities[city][2] - cities[city][0]) / N_REGIONS_SIDE
-        lat_unit = (cities[city][3] - cities[city][1]) / N_REGIONS_SIDE
-        lines = f_samples.readlines()
-        for line in lines[1:]:
-            aer_image_name = line.strip().split(',')[0]
-            bbox = aer_image_name[:-4].split("_")[1:]
-            bbox = [float(edge) for edge in bbox]
+    if not os.path.exists(os.path.join(city_splits_path, "train.csv")):
+        with (
+            open(os.path.join(city_splits_path, "samples.csv"), "r") as f_samples, 
+            open(os.path.join(city_splits_path, "train.csv"), "w") as f_train, 
+            open(os.path.join(city_splits_path, "validation.csv"), "w") as f_val,
+            open(os.path.join(city_splits_path, "test.csv"), "w") as f_test,
+        ):
+            long_unit = (cities[city][2] - cities[city][0]) / N_REGIONS_SIDE
+            lat_unit = (cities[city][3] - cities[city][1]) / N_REGIONS_SIDE
+            lines = f_samples.readlines()
+            for line in lines[1:]:
+                aer_image_name = line.strip().split(',')[0]
+                bbox = aer_image_name[:-4].split("_")[1:]
+                bbox = [float(edge) for edge in bbox]
 
-            heat_data.add(((bbox[3] + bbox[1]) / 2, (bbox[2] + bbox[0]) / 2))
+                heat_data.add(((bbox[3] + bbox[1]) / 2, (bbox[2] + bbox[0]) / 2))
 
-            long_gl_diff = (bbox[2] - bbox[0]) / 4
-            lat_gl_diff = (bbox[3] - bbox[1]) / 4
+                long_gl_diff = (bbox[2] - bbox[0]) / 4
+                lat_gl_diff = (bbox[3] - bbox[1]) / 4
 
-            left_region = int((bbox[0] + long_gl_diff - cities[city][0]) / long_unit)
-            bottom_region = int((bbox[1] + lat_gl_diff - cities[city][1]) / lat_unit)
-            right_region = int((bbox[2] - long_gl_diff - cities[city][0]) / long_unit)
-            top_region = int((bbox[3] - lat_gl_diff - cities[city][1]) / lat_unit)
+                left_region = int((bbox[0] + long_gl_diff - cities[city][0]) / long_unit)
+                bottom_region = int((bbox[1] + lat_gl_diff - cities[city][1]) / lat_unit)
+                right_region = int((bbox[2] - long_gl_diff - cities[city][0]) / long_unit)
+                top_region = int((bbox[3] - lat_gl_diff - cities[city][1]) / lat_unit)
 
-            corners = [
-                (left_region, top_region),
-                (left_region, bottom_region),
-                (right_region, top_region),
-                (right_region, bottom_region),
-            ]
+                corners = [
+                    (left_region, top_region),
+                    (left_region, bottom_region),
+                    (right_region, top_region),
+                    (right_region, bottom_region),
+                ]
 
-            if all(corner in train for corner in corners):
-                f_train.write(line)
-            elif all(corner in val for corner in corners):
-                f_val.write(line)
-            elif all(corner in test for corner in corners):
-                f_test.write(line)
-            else:
-                unused_count += 1
+                if all(corner in train for corner in corners):
+                    f_train.write(line)
+                elif all(corner in val for corner in corners):
+                    f_val.write(line)
+                elif all(corner in test for corner in corners):
+                    f_test.write(line)
+                else:
+                    unused_count += 1
 
-    print(f"Unused percentage in {city}: {unused_count / len(lines[1:]) * 100:.2f}%")
+        print(f"Unused percentage in {city}: {unused_count / len(lines[1:]) * 100:.2f}%")
 
-    grid_cells = []
-    grid_values = []
-    for i, x in enumerate(np.arange(cities[city][0], cities[city][2], long_unit)):
-        for j, y in enumerate(np.arange(cities[city][1], cities[city][3], lat_unit)):
-            grid_cells.append(box(x, y, x+long_unit, y+lat_unit))
-            if (i, j) in train:
-                grid_values.append("train")
-            elif (i, j) in val:
-                grid_values.append("val")
-            elif (i, j) in test:
-                grid_values.append("test")
-            else:
-                grid_values.append("unused")
+        grid_cells = []
+        grid_values = []
+        for i, x in enumerate(np.arange(cities[city][0], cities[city][2], long_unit)):
+            for j, y in enumerate(np.arange(cities[city][1], cities[city][3], lat_unit)):
+                grid_cells.append(box(x, y, x+long_unit, y+lat_unit))
+                if (i, j) in train:
+                    grid_values.append("train")
+                elif (i, j) in val:
+                    grid_values.append("val")
+                elif (i, j) in test:
+                    grid_values.append("test")
+                else:
+                    grid_values.append("unused")
 
-    grid = gpd.GeoDataFrame(geometry=grid_cells, crs="EPSG:4326")
-    grid['value'] = grid_values
+        grid = gpd.GeoDataFrame(geometry=grid_cells, crs="EPSG:4326")
+        grid['value'] = grid_values
 
-    # Create folium map
-    m = folium.Map(location=[(cities[city][3] + cities[city][1]) / 2, (cities[city][2] + cities[city][0]) / 2], zoom_start=11)
+        # Create folium map
+        m = folium.Map(location=[(cities[city][3] + cities[city][1]) / 2, (cities[city][2] + cities[city][0]) / 2], zoom_start=11)
 
-    # Create heatmap overlay
-    heatmap = folium.FeatureGroup(name='Heatmap')
-    HeatMap(heat_data).add_to(heatmap)
-    heatmap.add_to(m)
+        # Create heatmap overlay
+        heatmap = folium.FeatureGroup(name='Heatmap')
+        HeatMap(heat_data).add_to(heatmap)
+        heatmap.add_to(m)
 
-    # Create train/val/test overlay
-    grid_overlay = folium.FeatureGroup(name='Train/Val/Test Split')
-    
-    # Add polygons to grid overlay
-    for _, row in grid.iterrows():
-        sim_geo = gpd.GeoSeries([row['geometry']]).__geo_interface__['features'][0]['geometry']
+        # Create train/val/test overlay
+        grid_overlay = folium.FeatureGroup(name='Train/Val/Test Split')
         
-        # Define colors based on value
-        if row['value'] == "train":
-            color_hex = '#0000FF'  # blue
-        elif row['value'] == "val":
-            color_hex = '#FFA500'  # orange
-        elif row['value'] == "test":
-            color_hex = '#00FF00'  # green
-        else:  # unused
-            color_hex = '#FFFFFF'  # white
+        # Add polygons to grid overlay
+        for _, row in grid.iterrows():
+            sim_geo = gpd.GeoSeries([row['geometry']]).__geo_interface__['features'][0]['geometry']
             
-        folium.GeoJson(
-            sim_geo,
-            style_function=lambda feature, color=color_hex: {
-                'fillColor': color,
-                'color': 'black',
-                'weight': 0.5,
-                'fillOpacity': 0.6,
-            },
-            tooltip=f"Value: {row['value']}"
-        ).add_to(grid_overlay)
+            # Define colors based on value
+            if row['value'] == "train":
+                color_hex = '#0000FF'  # blue
+            elif row['value'] == "val":
+                color_hex = '#FFA500'  # orange
+            elif row['value'] == "test":
+                color_hex = '#00FF00'  # green
+            else:  # unused
+                color_hex = '#FFFFFF'  # white
+                
+            folium.GeoJson(
+                sim_geo,
+                style_function=lambda feature, color=color_hex: {
+                    'fillColor': color,
+                    'color': 'black',
+                    'weight': 0.5,
+                    'fillOpacity': 0.6,
+                },
+                tooltip=f"Value: {row['value']}"
+            ).add_to(grid_overlay)
+        
+        grid_overlay.add_to(m)
+
+        # Add layer control to toggle overlays
+        folium.LayerControl().add_to(m)
+
+        m.save(os.path.join(city_splits_path, "grid_cells.html"))
     
-    grid_overlay.add_to(m)
+    if not os.path.exists(os.path.join(city_splits_path, "comp_train.csv")):
 
-    # Add layer control to toggle overlays
-    folium.LayerControl().add_to(m)
-
-    m.save(os.path.join(city_splits_path, "grid_cells.html"))
+        with (
+            open(os.path.join(city_splits_path, "train.csv"), "r") as f_train,
+            open(os.path.join(city_splits_path, "validation.csv"), "r") as f_val,
+            open(os.path.join(city_splits_path, "test.csv"), "r") as f_test,
+            open(os.path.join(city_splits_path, "comp_train.csv"), "w") as f_comp_train, 
+            open(os.path.join(city_splits_path, "comp_validation.csv"), "w") as f_comp_val,
+            open(os.path.join(city_splits_path, "comp_test.csv"), "w") as f_comp_test,
+        ):
+            df = pd.read_csv(os.path.join(city_splits_path, "ground_metadata.csv"))
+            for line in f_train:
+                line_list = line.split("'")
+                bbox = line_list[0][:-3].split
+                new_line_list = []
+                for gl in line_list[1:]:
+                    if 
